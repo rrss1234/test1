0380eb899ec58832bf50d79cccc42c1b5c05f8a4
bdlivestreemhd <cricketpagla135@gmail.com>
2025-06-09 12:05:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,24 @@
 const channels = [
-
   {
     name: "Gazi Tv",
     logo: "https://i.postimg.cc/CMrZ8hDz/Gtv.png",
-    url: "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDDEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFsaWRtaW51aiPhnPTI2/gazibdz.stream/playlist.m3u8"
+    url: "https://app24.jagobd.com.bd/.../gazibdz.stream/playlist.m3u8"
   },
   {
     name: "Channel 9",
     logo: "https://i.postimg.cc/0yDrh74w/Channel-9.png",
-    url: "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDDEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFsaWRtaW51aiPhnPTI2/channel9hd.stream/playlist.m3u8"
+    url: "https://app24.jagobd.com.bd/.../channel9hd.stream/playlist.m3u8"
   },
   {
     name: "Channel I",
     logo: "https://i.postimg.cc/52d256tN/Channel-i.png",
-    url: "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDDEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFsaWRtaW51aiPhnPTI2/channeli-8-org.stream/playlist.m3u8"
+    url: "https://app24.jagobd.com.bd/.../channeli-8-org.stream/playlist.m3u8"
   },
-  {
-    name: "Nexus Television",
-    logo: "https://www.jagobd.com/wp-content/uploads/2021/07/nexustv.png",
-    url: "https://app24.jagobd.com.bd/c3VydmVyX8RpbEU9Mi8xNy8yMFDDEHGcfRgzQ6NTAgdEoaeFzbF92YWxIZTO0U0ezN1IzMyfvcEdsEfeDeKiNkVN3PTOmdFsaWRtaW51aiPhnPTI2/nexustv.stream/playlist.m3u8"
-  },
-  {
-    name: "All Time Movies",
-    logo: "https://i.postimg.cc/nVNDJ5bd/20250522-024451.jpg",
-    url: "https://cloudfrontnet.vercel.app/tplay/playout/209612/master.m3u8"
-  },
-  {
-    name: "Bangla Drams",
-    logo: "https://i.postimg.cc/G20mh9Lj/20250522-025124.jpg",
-    url: "https://cloudfrontnet.vercel.app/tplay/playout/209593/master.m3u8"
-  },
-  {
-    name: "Ahil Tv HD",
-    logo: "https://i.postimg.cc/6qPT58m2/20250522-024115.jpg",
-    url: "https://padmaonline.duckdns.org:8088/restream3/index.m3u8"
-  },
-  {
-    name: "Bangla Music",
-    logo: "https://i.postimg.cc/XYc8gBhr/20250522-025321.jpg",
-    url: "https://cloudfrontnet.vercel.app/tplay/playout/209587/master.m3u8"
-  },
-  {
-    name: "Hindi Music",
-    logo: "https://i.postimg.cc/mkS3kyGX/20250522-030456.jpg",
-    url: "https://cloudfrontnet.vercel.app/tplay/playout/209592/master.m3u8"
-  },
-{
-    name: "Gopal Bhar",
-    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUrBhuJjFmg5TZk7hVQEBtoOs7ejO-gwOOLLe9IAfZvsBblMLMZc0ZBsM&s=10",
-    url: "https://cloudfrontnet.vercel.app/tplay/playout/209611/master.m3u8"
-  },
-{
-    name: "Bangla Waz",
-    logo: "https://i.postimg.cc/26DBWLGP/20250522-030650.jpg",
-    url: "https://cloudfrontnet.vercel.app/tplay/playout/209617/master.m3u8"
-},
-  {
-    name: "Bangla Kirtan",
- logo: "https://i.postimg.cc/HWJVL2TV/20250522-030919.jpg",
-    url: "https://cloudfrontnet.vercel.app/tplay/playout/209618/master.m3u8"
-  }
-
+  // ... আরও চ্যানেলগুলো এখানে থাকবে ...
 ];
+
+// 🔁 এই URL টা হবে সব চ্যানেলের জন্য কমন লোগো (প্লে শুরুর আগে দেখা যাবে)
+const defaultPoster = "https://i.postimg.cc/Gt7rd4zD/20250609-120504.jpg";
 
 let currentChannel = 0;
 let player = null;
@@ -74,14 +31,14 @@ function loadChannel(index) {
 
   setTimeout(() => {
     player = new Clappr.Player({
-  source: channel.url,
-  parentId: "#player",
-  poster: channel.logo,
-  width: "100%",
-  height: "100%",
-  autoPlay: true,
-  mute: false,
-});
+      source: channel.url,
+      parentId: "#player",
+      poster: defaultPoster, // সব চ্যানেলের জন্য একই পোস্টার
+      width: "100%",
+      height: "100%",
+      autoPlay: true,
+      mute: false,
+    });
     document.getElementById("player").style.opacity = 1;
   }, 300);
 }
